7e3d9c2a50f4f571c126a8d7e4b39ea102bf5b96
Pro-gram <stevenjeffers@live.ca>
2023-12-12 16:27:36 -0500

    update 1

---
+++ app/comps/Hbtn.js
@@ -0,0 +1,14 @@
+import './hbtn.css'; 
+
+function Hbtn() {
+
+  return (
+    <div className="btnCont">
+      <button className="glass">
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default Hbtn;
