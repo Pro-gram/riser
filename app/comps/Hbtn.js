add8c2ce680cc99a2fe9f97c3943a9eaa86f0eee
Pro-gram <stevenjeffers@live.ca>
2023-12-13 19:47:56 -0500

    update 2

--- app/comps/Hbtn.js
+++ app/comps/Hbtn.js
@@ -1,12 +1,20 @@
-import './hbtn.css'; 
+// Hbtn.js
+import React from "react";
+import "./hbtn.css";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 function Hbtn() {
-
   return (
-    <div className="btnCont">
-      <button className="glass">
-        Click me
-      </button>
+    <div className="inputContainer">
+      <input
+        type="text"
+        name="search"
+        className="inputBox"
+        placeholder="Begin Your Search Here..."
+        autoComplete="off"
+      />
+      <i className="fa fa-angle-right"></i>
+      <div className="blinking-bar"></div>
     </div>
   );
 }
